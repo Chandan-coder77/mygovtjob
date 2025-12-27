77bd1f6dbc875f6ce60b34f4f3398e7b196fcfd1
Chandan-coder77 <7finder8@gmail.com>
2025-12-27 22:33:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-# main.py
-# -------------------------
-# Runs the scraper and updates jobs.json
-# Make sure scraper.py contains function => scrape_freejobalert()
-# -------------------------
+import json, os
+from scraper import scrape
 
-import json
-from scraper import scrape_freejobalert   # must match function name in scraper.py
+jobs_file = "jobs.json"
+sources_file = "sources.txt"
 
-def save_jobs(data, file="jobs.json"):
-    with open(file, "w", encoding="utf-8") as f:
+def load_sources():
+    if not os.path.exists(sources_file):
+        print("❌ sources.txt missing — Add job URLs inside sources.txt")
+        return []
+    with open(sources_file, "r") as file:
+        return [l.strip() for l in file if l.strip()]
+
+def load_old_jobs():
+    return json.load(open(jobs_file)) if os.path.exists(jobs_file) else []
+
+def save_jobs(data):
+    with open(jobs_file, "w") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 def main():
-    print("\n🚀 Running Government Job Scraper...\n")
+    print("\n🚀 Auto Job Scraper Running...\n")
+    sites = load_sources()
+    old = load_old_jobs()
+    new = []
 
-    jobs = scrape_freejobalert()   # <-- call scraper
-    save_jobs(jobs)
+    for url in sites:
+        print("🔍 Checking:", url)
+        jobs = scrape(url)
+        new.extend(jobs)
 
-    print(f"✔ Jobs updated successfully! Total Jobs: {len(jobs)}\n")
+    # remove duplicates by title
+    final = []
+    titles = set()
+
+    for job in new + old:
+        if job["title"] not in titles and job["qualification"] != "Updating Soon":
+            titles.add(job["title"])
+            final.append(job)
+
+    save_jobs(final)
+    print("✔ Updated Successfully | Total Jobs:", len(final))
 
 if __name__ == "__main__":
     main()
